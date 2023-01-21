f967a1a9a336528c6ab056d52643227bad1121fd
chandinithirumalai <chandini607@gmail.com>
2023-01-21 09:55:23 +0800

    added users app

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project, Pledge 
-from .serializers import ProjectSerializer, PledgeSerializer
-from rest_framework import status, generics 
+from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+from rest_framework import status, permissions, generics
 from django.http import Http404
+from .permissions import IsOwnerOrReadOnly
 
 
 
 
 
 # Create your views here.
+class PledgeList(generics.ListCreateAPIView):
+	queryset = Pledge.objects.all()
+	serializer_class = PledgeSerializer
+
 
 class ProjectList(APIView):
-
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     def get(self, request):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
@@ -25,25 +30,33 @@ class ProjectList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status= status.HTTP_201_CREATED)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class ProjectDetail(APIView):
     
-    def get_object(self,pk):
-        try: 
-            return Project.objects.get(pk=pk)
+    def get_object(self, pk):
+        try:
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request,project)
+            return project
         except Project.DoesNotExist:
             raise Http404 
 
     
-    def get(self, request,pk):
+    def get(self, request, pk):
         project = self.get_object(pk)
-        serializer = ProjectSerializer(project)
+        serializer = ProjectDetailSerializer (project)
         return Response(serializer.data)
 
+    
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(instance=project,data=data,partial=True)
+        if serializer.is_valid():serializer.save()
 
-class PledgeList(generics.ListCreateAPIView):
-    queryset = Pledge.objects.all()
-    serializer_class = PledgeSerializer
+
+
+
 
 
